a0057040bed0c23212eaff3e996e296076ddb999
jricardovelez <jricardo.velez@udea.edu.co>
2024-04-07 21:24:28 -0500

    se añadio actionlisteners a los componentes y la clase se extends a AbstractTableModel

--- Inventory of products/src/control/control_inventory.java
+++ Inventory of products/src/control/control_inventory.java
@@ -6,31 +6,84 @@ package control;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+import javax.swing.table.AbstractTableModel;
 import model.Inventory;
 import model.Product;
 import view.frmInventory;
 
 
-public class control_inventory implements ActionListener{
+public class control_inventory  extends AbstractTableModel implements ActionListener {
 
-    private frmInventory frmInventory;
-    private Product product;
-    private Inventory inventory;
+    private final frmInventory frmInventory;
+    private final Inventory inventory;
 
-    public control_inventory(frmInventory frmInventory, Product product, Inventory inventory) {
+    public control_inventory(frmInventory frmInventory,Inventory inventory) {
         this.frmInventory = frmInventory;
-        this.product = product;
         this.inventory = inventory;
+        this.frmInventory.btnAdd.addActionListener(this);
+        this.frmInventory.btnDelete.addActionListener(this);
+        this.frmInventory.btnSearch.addActionListener(this);
+        this.frmInventory.btnUpdate.addActionListener(this);
     }
     
     
     @Override
     public void actionPerformed(ActionEvent e) {
         
+        if(e.getSource() == this.frmInventory.btnAdd){
+            if(validateFields()){
+                if(inventory.check_if_product_exists(this.frmInventory.txtName.getText())){
+                    JOptionPane.showMessageDialog(null,"the product is already in inventory");
+                }
+                else{
+                    String name = this.frmInventory.txtName.getText();
+                    double price= Double.parseDouble(this.frmInventory.txtPrice.getText());
+                    int stock = Integer.parseInt(this.frmInventory.txtStock.getText());        
+                    this.inventory.add_product(new Product (name, price, stock));
+                }
+            }
+        }
+        
         
         
     }
+
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
+    // validar que los campos no esten vacios y tengan un tipo correcto
+    private boolean validateFields() {
+        // Verificar que los campos no estén vacíos
+        if (this.frmInventory.txtName.getText().isEmpty() || this.frmInventory.txtPrice.getText().isEmpty() || this.frmInventory.txtStock.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null,"Fill out all fields"); 
+            return false;
+        }
+
+        // Verificar que el precio y el stock sean números válidos
+        try {
+            Double.valueOf(this.frmInventory.txtPrice.getText());
+            Integer.valueOf(this.frmInventory.txtStock.getText());
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null,"Enter a correct number for price or stock"); 
+            return false;
+        }
+
+        // Todos los campos son válidos
+        return true;
+    } 
     
     
 }
