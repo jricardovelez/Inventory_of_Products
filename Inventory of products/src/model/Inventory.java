6f8f88ef5ef9558fe1a6c1857cc60e3611c2bf77
jricardovelez <jricardo.velez@udea.edu.co>
2024-04-07 19:44:59 -0500

    se agrego los metodos agregar, eliminar, actualizar, y buscar

--- Inventory of products/src/model/Inventory.java
+++ Inventory of products/src/model/Inventory.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 public class Inventory {
     
     private ArrayList <Product> list_products ;
-    private double price_total;
    
 
     public Inventory(ArrayList<Product> list_products) {
@@ -23,6 +22,52 @@ public class Inventory {
 
     public void setList_products(ArrayList<Product> list_products) {
         this.list_products = list_products;
+    }
+    
+    public boolean check_if_product_exists (String name){
+        
+        boolean is_present = false;
+        for (Product producto : this.list_products) {
+            if (producto.getName().equalsIgnoreCase(name)) {// comparar la lista con el nombre del producto sin importar mayusulas o minusculas
+                is_present = true; // si el producto ya esta retorna true
+                break;
+            }
+        }
+            return is_present;
+    }
+    
+    public void add_product (Product product){
+        this.list_products.add(product);
+    }
+    
+    public void remove_product (int index){
+        this.list_products.remove(index);
+    }
+    
+    public void update_product (int index, Product product){
+        this.list_products.set(index, product);
+    }
+    
+    public ArrayList<Product> search_products(String search_parameter) {
+        // Crear una nueva lista para almacenar los productos encontrados
+        ArrayList<Product> products_found = new ArrayList<>();
+
+        // Iterar sobre la lista de productos y buscar coincidencias
+        for (Product product : this.list_products) {
+            if (product.getName().toLowerCase().contains(search_parameter.toLowerCase())||
+            String.valueOf(product.getPrice()).contains(search_parameter) ||
+            String.valueOf(product.getStock()).contains(search_parameter)) {
+                // Si se encuentra una coincidencia, agregar el producto a la lista de productos encontrados
+                products_found.add(product);
+            }
+        }
+
+        // Retornar la lista de productos encontrados // mientras esta no este vacia
+        if(!products_found.isEmpty()){
+            return products_found;
+        }else{
+            return this.list_products; // si esta vacia la lista products_found retorna la lista original
+        }
     }
     
 }
